583a910382bfdf03e9efc4b6a37761b91f0c84ca
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-06-22 22:22:45 +0530

    Fix profile link path in admin header
    Updated the profile link in the admin header from '../profile.php' to 'profile.php' to correct the navigation path for user profile access.

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -173,10 +173,9 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] != 'admin') {
                                 <img class="img-profile rounded-circle"
                                     src="https://ui-avatars.com/api/?name=<?php echo isset($user['name']) ? urlencode($user['name']) : 'Admin+User'; ?>&background=random">
                             </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                            <!-- Dropdown - User Information -->                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="../profile.php">
+                                <a class="dropdown-item" href="profile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
